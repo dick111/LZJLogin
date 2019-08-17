3fe6a1cf1c489c2ac03e0ad9790d2460d8789e69
zhiji.liang <zhiji.liang@ubtrobot.com>
2019-08-17 14:15:39 +0800

    init project

---
+++ Example/Pods/Target Support Files/Pods-LZJLogin_Example/Pods-LZJLogin_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LZJLogin_Example : NSObject
+@end
+@implementation PodsDummy_Pods_LZJLogin_Example
+@end
---
+++ LZJLogin/Classes/LZJLogin.swift
@@ -0,0 +1,12 @@
+//
+//  LZJLogin.swift
+//  FBSnapshotTestCase
+//
+//  Created by LZJ on 2019/8/17.
+//
+
+import UIKit
+
+class LZJLogin: NSObject {
+
+}
