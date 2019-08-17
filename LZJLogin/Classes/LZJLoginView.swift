c72d4cb09da933d326b2c980378f018a31f78986
zhiji.liang <zhiji.liang@ubtrobot.com>
2019-08-17 14:46:34 +0800

    commit file

---
+++ LZJLogin/Classes/LZJLoginView.swift
@@ -0,0 +1,14 @@
+//
+//  LZJLoginView.swift
+//  PodsDemo
+//
+//  Created by LZJ on 2019/8/17.
+//  Copyright © 2019 LZJ. All rights reserved.
+//
+
+import UIKit
+
+class LZJLoginView: NSObject {
+
+    var loginView: UIView?
+}
